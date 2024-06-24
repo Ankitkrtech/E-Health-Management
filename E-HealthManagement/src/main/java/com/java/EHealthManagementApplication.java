f9771c27958204bab92bb343605a56ab5156fc83
Ankit kumar <ankitkumarnau@gmail.com>
2024-06-24 17:49:14 +0530

    Project Configured

---
+++ E-HealthManagement/src/main/java/com/java/EHealthManagementApplication.java
@@ -0,0 +1,13 @@
+package com.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EHealthManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EHealthManagementApplication.class, args);
+	}
+
+}
